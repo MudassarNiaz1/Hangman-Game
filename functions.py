d9bbfeddc38ff34b2d5e9723580ac778d5a57aa9
Mudassar Niaz <159882845+MudassarNiaz1@users.noreply.github.com>
2024-11-06 22:36:21 +0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -31,42 +31,7 @@ class HangmanGame:
        self.wrong_guess = 0
        self.guessed_letters = []
        self.is_running = True
-
-
-    def main(self):
-        while self.is_running:
-            self.display_man()
-            self.display_hint()
-            guess_word = input("Enter a letter: ").lower()
-
-            if len(guess_word) != 1 or not guess_word.isalpha():
-                print("Please enter a single letter.")
-                continue
-
-            if guess_word in self.guessed_letters:
-                print(f"{guess_word} has already been guessed.")
-                continue
-
-            self.guessed_letters.append(guess_word)
-
-            if guess_word in self.answer:
-                for i in range(len(self.answer)):
-                    if self.answer[i] == guess_word:
-                        self.hint[i] = guess_word
-            else:
-                self.wrong_guess += 1
-
-            if "_" not in self.hint:
-                self.display_man()
-                self.display_answer()
-                print("Congratulations, you won!")
-                self.is_running = False
-            elif self.wrong_guess >= len(self.hangman_art) - 1:
-                self.display_man()
-                self.display_answer()
-                print(f"Game Over, you lost! The word was '{self.answer}'.")
-                self.is_running = False
-
+       
 
     def display_man(self):
         print("****Welcome to the Game****")
