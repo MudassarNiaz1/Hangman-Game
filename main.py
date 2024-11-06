038dcb3a0e3171fc25a591ffc8b2c2ea02a38c81
Mudassar Niaz <159882845+MudassarNiaz1@users.noreply.github.com>
2024-11-06 22:37:17 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,44 @@
 import random
 
 from functions import HangmanGame
-from words_list import words
+
+
+def main():
+  game = HangmanGame()
+  while game.is_running:
+      game.display_man()
+      game.display_hint()
+ #  game.display_answer()
+      guess_word = input("Enter a letter: ").lower()
+
+      if len(guess_word) != 1 or not guess_word.isalpha():
+       print("Please enter a single letter.")
+       continue
+
+      if guess_word in game.guessed_letters:
+       print(f"{guess_word} has already been guessed.")
+       continue
+
+      game.guessed_letters.append(guess_word)
+
+      if guess_word in game.answer:
+        for i in range(len(game.answer)):
+          if game.answer[i] == guess_word:
+            game.hint[i] = guess_word
+      else:
+         game.wrong_guess += 1
+
+      if "_" not in game.hint:
+                game.display_man()
+                game.display_answer()
+                print("Congratulations, you won!")
+                game.is_running = False
+      elif game.wrong_guess >= len(game.hangman_art) - 1:
+         game.display_man()
+         game.display_answer()
+         print(f"Game Over, you lost! The word was '{game.answer}'.")
+         game.is_running = False
+
 
 if __name__ == '__main__':
-    game = HangmanGame()
-    game.main()
+    main()
